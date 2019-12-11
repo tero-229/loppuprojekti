b7ef0728f5b8fc008b6adaac7b4abe1053e0262b
Kaldric <55188640+Kaldric@users.noreply.github.com>
2019-12-11 10:56:57 +0200

    Testin aiheita lisätty kommenttina, pieniä korjauksia UserControlleriin

--- src/test/java/academy/ystavasovellus/YstavasovellusApplicationTests.java
+++ src/test/java/academy/ystavasovellus/YstavasovellusApplicationTests.java
@@ -9,7 +9,11 @@ class YstavasovellusApplicationTests {
 	@Test
 	void contextLoads() {
 
+	}
+
+	//Mahdollisia testejä: 1) yhteys DB:hen toimii (esim. getillä), 2) listAllUsers-funktion tuoma lista on saman mittainen kuin DB, 3) edit-toiminnallisuus toimii eli vanha vs uusi tieto, 4) filtterit toimivat
+
 
 	}
 
-}
+
